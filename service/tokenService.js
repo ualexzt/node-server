9e175b8a37c4b6e4b30033edb8743f858d90772f
AlexUa <ualexzt@gmail.com>
2022-02-13 22:07:21 +0200

    add signup with jwt

---
+++ service/tokenService.js
@@ -0,0 +1,28 @@
+import Token from "../models/Token.js";
+import jwt from "jsonwebtoken";
+
+class TokenService {
+  // Generate JWT token
+  generateAccessToken(payload) {
+    const accesToken = jwt.sign(payload, process.env.JWT_ACCESS_TOKEN, {
+      expiresIn: "30m",
+    });
+    const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_TOKEN, {
+      expiresIn: "30d",
+    });
+    return { accesToken, refreshToken };
+  }
+
+  //Save token
+  async tokenSave(userId, refreshToken) {
+    const tokenData = await Token.findOne({ user: userId });
+    if (tokenData) {
+      tokenData.refreshToken = refreshToken;
+      return tokenData.save();
+    }
+    const token = await Token.create({ user: userId, refreshToken });
+    return token;
+  }
+}
+
+export default new TokenService();
