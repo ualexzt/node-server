8b006035b3deb54bde779deec62849c37931036d
AlexUa <ualexzt@gmail.com>
2022-02-14 14:37:00 +0200

    review code

--- service/tokenService.js
+++ service/tokenService.js
@@ -4,13 +4,13 @@ import jwt from "jsonwebtoken";
 class TokenService {
   // Generate JWT token
   generateTokens(payload) {
-    const accesToken = jwt.sign(payload, process.env.JWT_ACCESS_TOKEN, {
+    const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_TOKEN, {
       expiresIn: "30m",
     });
     const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_TOKEN, {
       expiresIn: "30d",
     });
-    return { accesToken, refreshToken };
+    return { accessToken, refreshToken };
   }
 
   //Save token
